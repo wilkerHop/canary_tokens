7a0a5576318a40fd85c4eeebf42d442909677b8e
Wilker Ribeiro <rekliw97@gmail.com>
2019-01-27 17:43:51 -0200

    added dog route

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const path = require('path')
+const fs = require('fs');
 
 app.use(bodyParser.json())
 
 app.post('/', async (req, res) => {
     insert(req.body)
-    
+
     res.sendStatus(200)
-
 })
 
-app.get('/dog.jpg',(req,res)=>{
-    insert(req.connection.localAddress,
-        req.headers["user-agent"],
-        new Date(),
-        req.cookies,
-        req.params.urlCode)
-    res.sendFile('./dog.jpg')
+app.get('/dog.jpg', async(req, res) => {
+    await insert({
+        address: req.connection.localAddress,
+        'user-agent': req.headers["user-agent"],
+        date: new Date(),
+        cookies: req.cookies,
+    })
+    console.log(path.join(__dirname, 'dog.jpg'))
+    res.sendFile(path.join(__dirname, 'dog.jpg'))
 })
-const insert = async (obj)=>{
-    const db = await MongoClient.connect(require(`./consfig/database`))
+
+const insert = async (obj) => {
+    const db = await MongoClient.connect(require(`./config/database`), { useNewUrlParser: true })
     const dbo = db.db('canary_tokens')
-    await dbo.collection('tokens').insert(obj)
+    await dbo.collection('tokens').insertOne(obj)
     db.close()
 }
-app.listen(process.env.PORT || 3000, console.log('server up'))  
+app.listen(process.env.PORT || 3000, console.log('server up'))
