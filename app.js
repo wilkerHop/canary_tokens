db681da2533a7b0c0d4c7f5170fe5bba34d1a601
Wilker Ribeiro <rekliw97@gmail.com>
2019-01-27 05:43:35 -0200

    cleaning code

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use(bodyParser.json())
 app.post('/', async (req, res) => {
     const db = await MongoClient.connect(require(`./consfig/database`))
     const dbo = db.db('canary_tokens')
-    const r = await dbo.collection('tokens').insert(req.body)
-    res.send({ insertResponse: r, token: req.body })
+    await dbo.collection('tokens').insert(req.body)
+    db.close()
+    res.sendStatus(200)
+
 })
 
 app.listen(process.env.PORT || 3000, console.log('server up'))  
