553f7aa27ee756e8c7e3ca6cf17c1b9e48a404f2
Wilker Ribeiro <rekliw97@gmail.com>
2019-01-27 05:48:14 -0200

    image route added

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ const MongoClient = require('mongodb').MongoClient
 app.use(bodyParser.json())
 
 app.post('/', async (req, res) => {
-    const db = await MongoClient.connect(require(`./consfig/database`))
-    const dbo = db.db('canary_tokens')
-    await dbo.collection('tokens').insert(req.body)
-    db.close()
+    insert(req.body)
+    
     res.sendStatus(200)
 
 })
 
+app.get('/dog.jpg',(req,res)=>{
+    insert(req.connection.localAddress,
+        req.headers["user-agent"],
+        new Date(),
+        req.cookies,
+        req.params.urlCode)
+    res.sendFile('./dog.jpg')
+})
+const insert = async (obj)=>{
+    const db = await MongoClient.connect(require(`./consfig/database`))
+    const dbo = db.db('canary_tokens')
+    await dbo.collection('tokens').insert(obj)
+    db.close()
+}
 app.listen(process.env.PORT || 3000, console.log('server up'))  
