83ef7b45adf08e8a716fff72590bd469d9ab1d14
Wilker Ribeiro <rekliw97@gmail.com>
2019-02-02 11:01:02 -0200

    get express ip info

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
+const expressip = require('express-ip')
 const MongoClient = require('mongodb').MongoClient
 const path = require('path')
 const fs = require('fs');
 
 app.use(bodyParser.json())
-
+app.use(expressip().getIpInfoMiddleware)
 app.post('/', async (req, res) => {
     insert(req.body)
 
     res.sendStatus(200)
 })
 
-app.get('/dog.jpg', async(req, res) => {
+app.get('/dog.jpg', async (req, res) => {
+    console.log('expressip', req.ipInfo)
     await insert({
         address: req.connection.localAddress,
         date: new Date(),
         cookies: req.cookies,
-        headers:req.headers,
-        hostname:req.hostname,
+        headers: req.headers,
+        hostname: req.hostname,
+        ipInfo: req.ipInfo
         // connection:JSON.stringify(req.connection),
     })
     res.sendFile(path.join(__dirname, 'dog.jpg'))
 })
 
 const insert = async (obj) => {
-    const db = await MongoClient.connect(require(`./config/database`), { useNewUrlParser: true })
+    const db = await MongoClient.connect(require(`./config/database`),
+        { useNewUrlParser: true })
     const dbo = db.db('canary_tokens')
     await dbo.collection('tokens').insertOne(obj)
     db.close()
 }
+
 app.listen(process.env.PORT || 3000, console.log('server up'))
