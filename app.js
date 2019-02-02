0ab1d8ec43b5cf865145028e8f2331a2caabc421
Wilker Ribeiro <rekliw97@gmail.com>
2019-02-02 11:09:09 -0200

    add flower.png route

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const app = require('express')()
 const expressip = require('express-ip')
 const MongoClient = require('mongodb').MongoClient
 const path = require('path')
-const fs = require('fs');
 
 app.use(expressip().getIpInfoMiddleware)
 
@@ -18,6 +17,19 @@ app.get('/dog.jpg', async (req, res) => {
         // connection:JSON.stringify(req.connection),
     })
     res.sendFile(path.join(__dirname, 'dog.jpg'))
+})
+
+app.get('/flower.png', (req, res) => {
+    await insert({
+        address: req.connection.localAddress,
+        date: new Date(),
+        cookies: req.cookies,
+        headers: req.headers,
+        hostname: req.hostname,
+        ipInfo: req.ipInfo
+        // connection:JSON.stringify(req.connection),
+    })
+    res.sendFile('https://cdn.pixabay.com/photo/2018/03/02/08/47/rose-3192610_960_720.png')
 })
 
 const insert = async (obj) => {
