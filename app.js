d507bd2f3b4749d94b1f45613ce62effdfd89dff
Wilker Ribeiro <rekliw97@gmail.com>
2019-02-17 21:35:21 -0300

    added git redirect

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const app = require('express')() // express server
 const expressip = require('express-ip') // lib for more user info
 const MongoClient = require('mongodb').MongoClient // MongoDB driver
 const path = require('path') // native path controll driver
-const urlLink = 'https://docs.google.com/presentation/d/1Ryo64FmYSiQPRT7CdavwOkevNBikc4mjkoVOKcrZCEw/edit?usp=sharing'
+const slideLink = 'https://docs.google.com/presentation/d/1Ryo64FmYSiQPRT7CdavwOkevNBikc4mjkoVOKcrZCEw/edit?usp=sharing'
 const binaryString = '47494638396101000100800000dbdfef00000021f90401000000002c00000000010001000002024401003b'
+const gitLink = 'https://github.com/wilkerHop/canary_tokens'
 
 app.use(expressip().getIpInfoMiddleware) // configures express-ip into express
 
@@ -13,8 +14,9 @@ app.get('**', async (req, res) => {
     case '/dog.jpg':
       return res.sendFile(path.join(__dirname, 'dog.jpg')) // renders an image
     case '/slide':
-      const slide = urlLink
-      return res.redirect(slide) // redirects to another external link
+      return res.redirect(slideLink) // redirects to another external link
+    case 'git':
+      return res.redirect(gitLink) // redirects to another external link
     default:
       const imgBinary = Buffer.alloc(6, binaryString, 'hex') // creates a buffer image
       res.write(imgBinary) // writes the buffer in the response body
