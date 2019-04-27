530e0693bd3dc863b94f3b890478b62f759567cf
Wilker Ribeiro <rekliw97@gmail.com>
2019-04-27 03:24:24 -0300

    base64 image

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const expressip = require('express-ip') // lib for more user info
 const MongoClient = require('mongodb').MongoClient // MongoDB driver
 const path = require('path') // native path controll driver
 const slideLink = 'https://docs.google.com/presentation/d/1Ryo64FmYSiQPRT7CdavwOkevNBikc4mjkoVOKcrZCEw/edit?usp=sharing'
-const binaryString = '47494638396101000100800000dbdfef00000021f90401000000002c00000000010001000002024401003b'
+const base64string = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
 const gitLink = 'https://github.com/wilkerHop/canary_tokens'
 
 app.use(expressip().getIpInfoMiddleware) // configures express-ip into express
@@ -12,15 +12,21 @@ app.get('**', async (req, res) => {
   await insert(req) // inserts request into database
   switch (req.url) { // checks url
     case '/dog.jpg':
-      return res.sendFile(path.join(__dirname, 'dog.jpg')) // renders an image
+      const dog = path.join(__dirname, 'dog.jpg')
+      return res.sendFile(dog) // renders an image
     case '/slide':
       return res.redirect(slideLink) // redirects to another external link
     case '/git':
       return res.redirect(gitLink) // redirects to another external link
     default:
-      const imgBinary = Buffer.alloc(6, binaryString, 'hex') // creates a buffer image
-      res.write(imgBinary) // writes the buffer in the response body
-      return res.send() // and sends it
+      res.write(
+        Buffer.alloc(
+          base64string.length,
+          base64string,
+          'base64'
+        )
+      )
+      return res.send()
   }
 })
 
@@ -34,11 +40,10 @@ const insert = async req => {
     hostname: req.hostname, // this server domain,
     ipInfo: req.ipInfo // and express-ip juice
   }
-  
+
   const con = await MongoClient.connect(require(`./config/database`), // connects to mLab
     { useNewUrlParser: true }) // using safer url parser
-  const dbo = con.db('canary_tokens')
-  await dbo.collection('tokens').insertOne(insert);
+  await con.db('canary_tokens').collection('tokens').insertOne(insert);
   con.close() // closes the connection with mLab
 }
 
